db80b11f2722b80470c9bd55b009c1909e8d8427
("SUMAN KAR") <145581646+Karsuman4298@users.noreply.github.com>
2026-04-25 23:46:15 +0530

    Add print statements for DLL insert operations

---
+++ DLL.py
@@ -0,0 +1,194 @@
+class Node:
+    def __init__(self,value):
+        self.prev=None
+        self.value=value
+        self.next=None
+        
+        
+class DoublyLinkedList:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+
+    def print_list(self):
+        temp=self.head
+        while temp:
+            print(temp.value)
+            temp=temp.next  
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.length==0:
+            self.head=new_node
+            self.tail=new_node
+
+        else:
+            new_node.prev=self.tail
+            self.tail.next=new_node
+            self.tail=new_node
+
+        self.length+=1      
+
+    def pop(self):
+        if self.length==0:
+            return None
+        
+        temp=self.tail
+
+        if self.length==1:
+            self.head=None
+            self.tail=None
+
+        else:
+            self.tail=self.tail.prev
+            self.tail.next=None
+            temp.prev=None    
+            self.length-=1
+
+        return temp   
+    
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.length ==0:
+            self.head=new_nodes
+            self.tail=new_node
+
+        new_node.next=self.head
+        self.head.prev=new_node
+        self.head=self.head.prev    
+        self.length+=1
+
+    def pop_first(self):
+    
+        if self.length==0:
+            return None
+        
+        temp=self.head
+
+        if self.length==1:
+            self.head=None
+            self.tail=None
+
+        else:
+            self.head=temp.next
+            temp.next=None
+            self.head.prev=None
+        self.length-=1
+        return temp    
+    
+    def get(self,index):
+        if index<0  or index>=self.length:
+            return None
+        
+        if index<self.length/2:
+            temp=self.head
+            for _ in range(index):
+                temp=temp.next
+
+        else:
+            temp=self.tail
+            for _ in range(self.length-1,index,-1):
+                temp=temp.prev
+        return temp    
+
+
+    def set_value(self,index,value):
+        if index<0  or index>=self.length:
+            return None
+        
+        if index<self.length/2:
+            temp=self.head
+            for _ in range(index):
+                temp=temp.next
+
+        else:
+            temp=self.tail
+            for _ in range(self.length-1,index,-1):
+                temp=temp.prev
+
+        temp.value=value
+ 
+
+    def insert(self, index, value):
+        new_node = Node(value)
+
+        if index < 0 or index > self.length:
+            return None
+
+        if index == 0:
+            return self.prepend(value)
+
+        if index == self.length:
+            return self.append(value)
+
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
+    
+
+        
+
+
+
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(3)
+
+
+print('DLL before insert():')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.insert(1,2)
+
+print('\nDLL after insert(2) in middle:')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.insert(0,0)
+
+print('\nDLL after insert(0) at beginning:')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.insert(4,4)
+
+print('\nDLL after insert(4) at end:')
+my_doubly_linked_list.print_list()
+
+
+
+
+"""
+DLL before insert():
+1
+3
+
+DLL after insert(2) in middle:
+1
+2
+3
+
+DLL after insert(0) at beginning:
+0
+1
+2
+3
+
+DLL after insert(4) at end:
+0
+1
+2
+3
+4
+"""
